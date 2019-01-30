aec45a75db8d669217bda580a2ab2b4ed29f911e
Jessica Stephens <jeslstep1@gmail.com>
2019-01-29 18:20:54 -0600

    trying a different database connection config in order to connect to database on heroku

--- app/init.php
+++ app/init.php
@@ -1,6 +1,12 @@
 <?php
+// get heroku database url to connect database
+$url = parse_str(getenv($DATABASE_URL));
 
-
+// seperate aspects of database url
+$host = $url["host"];
+$username = $url["user"];
+$password = $url["pass"];
+$database = substr($url["path"], 1);
 // connect to postgreSQL database
-$dbconn = pg_connect("host=localhost dbname=php_todo")
+$dbconn = pg_connect("host=$host dbname=$database user=$username pass=$password ")
     or die('Could not connect: ' . pg_last_error());
