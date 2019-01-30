39fa98a788c357af30e1aa85ec05f1d5287f72ec
Jessica Stephens <jeslstep1@gmail.com>
2019-01-29 18:22:54 -0600

    restored config to previous state

--- app/init.php
+++ app/init.php
@@ -1,12 +1,6 @@
 <?php
-// get heroku database url to connect database
-$url = parse_str(getenv($DATABASE_URL));
 
-// seperate aspects of database url
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
+
 // connect to postgreSQL database
-$dbconn = pg_connect("host=$host dbname=$database user=$username pass=$password ")
+$dbconn = pg_connect("host=localhost dbname=php_todo")
     or die('Could not connect: ' . pg_last_error());
