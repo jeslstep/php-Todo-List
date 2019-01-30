1b03d7f04cf6fefc431ca3d7bb8db988e0e5dbd5
Jessica Stephens <jeslstep1@gmail.com>
2019-01-29 20:47:11 -0600

    added other option hopefully allowing connection to heroku database successful

--- app/init.php
+++ app/init.php
@@ -2,5 +2,6 @@
 
 
 // connect to postgreSQL database
-$dbconn = pg_connect("host=localhost dbname=php_todo")
+$dbconn = pg_connect("host=localhost dbname=php_todo") 
+    or $dbconn = pg_connect(getenv("DATABASE_URL"))
     or die('Could not connect: ' . pg_last_error());
