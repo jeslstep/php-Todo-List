74ec9937de76b3124a0b0717aa02ba3cc26e020f
Jessica Stephens <jeslstep1@gmail.com>
2019-01-29 22:56:16 -0600

    added code comment to clarify init.php file database connection

--- app/init.php
+++ app/init.php
@@ -1,7 +1,8 @@
 <?php
 
 
-// connect to postgreSQL database
-$dbconn = pg_connect("host=localhost dbname=php_todo") 
+// connect to local postgreSQL database
+$dbconn = pg_connect("host=localhost dbname=php_todo")
+    // connect heroku database
     or $dbconn = pg_connect(getenv("DATABASE_URL"))
     or die('Could not connect: ' . pg_last_error());
